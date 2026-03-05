91cc8fe397cc4e42bc773bad8fb33965795f2e57
Al Mun Tasir Mamun <almuntasirmamun2000@gmail.com>
2026-03-06 02:39:28 +0600

    fix cors for vercel

--- server/app.js
+++ server/app.js
@@ -16,12 +16,7 @@ const refundRoutes = require('./src/routes/refundRoutes');
 const app = express();
 
 // Middleware
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://192.168.0.114:3000'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
