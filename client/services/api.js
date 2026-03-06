670a1d8bee177c4b12d5487f7e4ae036199d500b
Al Mun Tasir Mamun <almuntasirmamun2000@gmail.com>
2026-03-06 20:49:00 +0600

    add api env

--- client/services/api.js
+++ client/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001/api'||'https://dentist-appointment-production.up.railway.app/api';
 
 /**
  * Get auth token from localStorage
